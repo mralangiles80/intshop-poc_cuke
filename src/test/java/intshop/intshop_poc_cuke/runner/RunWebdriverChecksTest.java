c7fbf1f0fad72105377c23fc0c2214e9f97aaf3c
Mark Capaldi <mc@heritageit.co.uk>
2017-11-11 13:54:58 +0000

    Had to change the format of the path to the feature files.
    The previous path doesn't work on a Mac.
    
    Does the new one work on Windows?

--- src/test/java/intshop/intshop_poc_cuke/runner/RunWebdriverChecksTest.java
+++ src/test/java/intshop/intshop_poc_cuke/runner/RunWebdriverChecksTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         format = { "pretty", "html:target/cucumber" },
         glue = { "intshop.intshop_poc_cuke.steps"},
-        features = "src\\test\\java\\intshop\\intshop_poc_cuke\\features\\smoke\\"
+        features = "src/test/java/intshop/intshop_poc_cuke/features/smoke"
 )
 
 public class RunWebdriverChecksTest {
